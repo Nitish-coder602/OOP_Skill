b194dec469d30d01015923e3217d306a32ca6850
NITISH <nitish.54868@gmail.com>
2025-08-26 12:36:54 +0530

    Add files via upload

---
+++ interface.java
@@ -0,0 +1,36 @@
+interface A {
+    void methodA();
+}
+
+// Second parent interface
+interface B {
+    void methodB();
+}
+
+// Child interface extends both A and B
+interface C extends A, B {
+    void methodC();
+}
+class MyClass implements C {
+    public void methodA() {
+        System.out.println("Method A implemented");
+    }
+
+    public void methodB() {
+        System.out.println("Method B implemented");
+    }
+
+    public void methodC() {
+        System.out.println("Method C implemented");
+    }
+}
+
+// Driver class
+public class Test {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.methodA();
+        obj.methodB();
+        obj.methodC();
+    }
+}
